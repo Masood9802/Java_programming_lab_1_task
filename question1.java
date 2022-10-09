1122d1dd81d872b7b80ef02aef229266f4eb2ec2
Masood9802 <115369765+Masood9802@users.noreply.github.com>
2022-10-09 10:28:20 -0700

    Add files via upload

---
+++ question1.java
@@ -0,0 +1,33 @@
+//Masood Ahmed 
+//2k22/cse/55
+//Question 1
+
+
+
+class Question1{
+
+
+public static void main(String[] args) 
+
+{
+
+
+System.out.println("These are integers variables:");
+	System.out.println("1 , 2, 3 , 4 , 5");
+
+System.out.println("These are long integers variables:");
+	
+System.out.println("10114 , 3456 , 9876");
+	System.out.println("These are float variables");
+	System.out.println("2.3 , 67.9 , 90.6");
+	System.out.println("These are double variables:");
+	System.out.println("23.444 , 768.99 , 452.24");
+	System.out.println("These are characters variables:");
+	System.out.println("a , b , c");
+	
+System.out.println("These are boolean variables");
+	System.out.println("false and true");
+	
+}
+
+}
